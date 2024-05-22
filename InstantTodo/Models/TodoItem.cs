6150dc590a9f13deaf5d714d747faa2429b2890a
Ricky Velis <37187134+rickyvelis@users.noreply.github.com>
2024-05-22 10:36:05 +0200

    Existing project added

---
+++ InstantTodo/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace InstantTodo.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime Created { get; set;}
+        public DateTime Updated { get; set;}
+
+    }
+}
---
+++ InstantTodo/Models/TodoList.cs
@@ -0,0 +1,11 @@
+﻿namespace InstantTodo.Models
+{
+    public class TodoList
+    {
+        public List<TodoItem> TodoItems { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}

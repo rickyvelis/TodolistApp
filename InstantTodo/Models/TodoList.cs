b0580de4623d87881a6fe88da7bac04af3316f8b
Ricky Velis <37187134+rickyvelis@users.noreply.github.com>
2024-05-22 12:21:44 +0200

    Added to be implemented methods necessary for lists display

--- InstantTodo/Models/TodoList.cs
+++ InstantTodo/Models/TodoList.cs
@@ -7,5 +7,15 @@
         public string Description { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
+
+        public string GetPassedTime()
+        {
+            return "3 days ago"; //Dummy code. Functionality to be implemented.
+        }
+
+        public string GetTasksPreview()
+        {
+            return "Apples, Bread..."; //Dummy code. Functionality to be implemented.
+        }
     }
 }

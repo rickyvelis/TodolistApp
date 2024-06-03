0789a34f686c025bf0e6ac8d89794e0093e3f083
Ricky Velis <37187134+rickyvelis@users.noreply.github.com>
2024-06-03 13:12:32 +0200

    Added default values to props for TodoItem

--- InstantTodo/Models/TodoItem.cs
+++ InstantTodo/Models/TodoItem.cs
@@ -3,11 +3,11 @@
     public class TodoItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool IsDone { get; set; }
-        public DateTime Created { get; set;}
-        public DateTime Updated { get; set;}
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public bool IsDone { get; set; } = false;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set;} = DateTime.Now;
 
     }
 }
